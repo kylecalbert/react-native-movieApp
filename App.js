18aac5c37be774c58c0382ecde4f074ce7025f54
Jess purvis <jess.purvis@and.digital>
2023-02-01 12:26:30 +0000

    Added in exercise 2 content

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+const URI =
+  'https://m.media-amazon.com/images/M/MV5BMWRiZGQ1NDMtODQ2OS00MDlhLWJkZGYtM2ZmNjlhZThjOWRmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_FMjpg_UX1000_.jpg';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
+      <Text>MOST POPULAR MOVIES</Text>
+      <ScrollView horizontal>
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const MovieCard = () => (
+  <View>
+    <Image
+      source={{
+        uri: URI,
+      }}
+      style={{width: 200, height: 350, marginRight: 8}}
+    />
+    <Text>Sing 2</Text>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
