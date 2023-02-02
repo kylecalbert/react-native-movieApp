e280c2e4c0be29d28ef5fcc91aaeed961764fed4
Jess purvis <jess.purvis@and.digital>
2023-02-02 16:59:22 +0000

    11 matches slides

--- src/screens/AccountSettings/AccountSettings.jsx
+++ src/screens/AccountSettings/AccountSettings.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import {useAuthorization} from '../../context/AuthorizationContext';
 
 const AccountSettings = () => {
+  const [_, setIsAuthorized] = useAuthorization();
+
   return (
     <SafeAreaView>
       <Text>Account Settings</Text>
+      <Text onPress={() => setIsAuthorized(false)}>Logout</Text>
     </SafeAreaView>
   );
 };
