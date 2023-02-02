2d5898df923352dd79140af805e040dd934efeb5
Jess purvis <jess.purvis@and.digital>
2023-02-02 09:31:31 +0000

    fini

--- src/screens/Movies/Movies.jsx
+++ src/screens/Movies/Movies.jsx
@@ -1,40 +1,92 @@
+import {useNavigation} from '@react-navigation/native';
 import React, {useState, useEffect} from 'react';
-import {View, Text, Image, FlatList} from 'react-native';
-import { getPopularMovies } from '../../api/movies';
+import {View, Text, Image, SafeAreaView, Pressable, FlatList, ScrollView} from 'react-native';
 
-import styles from './Movies.styles';
+import {getPopularMovies, getAllMovies} from '../../api/movies.js';
+
+import styles from './Movies.styles.js';
 
 const Movies = () => {
-  const [popularMovies, setPopularMovies] = useState([]);
+  const navigation = useNavigation();
+
+  const [popularMovies, setPopularMovies] = useState();
+  const [movies, setMovies] = useState();
 
   useEffect(() => {
     fetchPopularMovies();
+    fetchAllMovies();
   }, []);
 
   const fetchPopularMovies = async () => {
     const fetchedPopularMovies = await getPopularMovies();
-
     setPopularMovies(fetchedPopularMovies);
   };
 
-  const MovieCard = ({image, title}) => (
-    <View style={styles.movieColumnContainer}>
-      <Image source={{uri: image}} style={styles.movieColumnImage} />
-      <Text>{title}</Text>
-    </View>
+  const fetchAllMovies = async () => {
+    const fetchedMovies = await getAllMovies();
+    setMovies(fetchedMovies);
+  };
+
+  const MovieColumn = ({id, posterImage, title}) => (
+    <Pressable
+      style={styles.movieColumnContainer}
+      onPress={() => navigation.navigate('Movie')}>
+      <Image source={{uri: posterImage}} style={styles.movieColumnImage} />
+      <Text style={styles.movieColumnTitle}>{title}</Text>
+    </Pressable>
   );
 
+  const MovieRow = ({ image, title, characters, genre, release }) => {
+    return (
+    <View style={styles.movieRowContainer}>
+      <Image source={{ uri: image }} style={styles.movieRowImage} />
+      <View style={styles.movieInfoContainer}>
+        <View>
+          <Text style={styles.movieRowTitle}>{title}</Text>
+          <Text>{characters}</Text>
+        </View>
+        <View>
+          <Text>{genre}</Text>
+          <Text>{release}</Text>
+        </View>
+      </View>
+    </View>)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>MOST POPULAR MOVIES</Text>
-      <FlatList
-        data={popularMovies}
-        renderItem={({item}) => (
-          <MovieCard image={item.posterImage} title={item.title} />
-        )}
-        horizontal
-      />
-    </View>
+    <SafeAreaView style={{backgroundColor: 'white'}}>
+      <View style={styles.container}>
+        <ScrollView>
+          <Text style={styles.title}>MOST POPULAR MOVIES</Text>
+
+          <FlatList
+            data={popularMovies}
+            renderItem={value => (
+              <MovieColumn
+                id={value.item.id}
+                posterImage={value.item.posterImage}
+                title={value.item.title}
+              />
+            )}
+            horizontal
+          />
+
+          <Text style={styles.title}>ALL MOVIES</Text>
+          <FlatList
+            data={movies}
+            renderItem={value => (
+              <MovieRow
+                image={value.item.image}
+                title={value.item.title}
+                characters={value.item.characters}
+                genre={value.item.genre}
+                release={value.item.release}
+              />
+            )}
+          />
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- src/screens/Movies/Movies.styles.js
+++ src/screens/Movies/Movies.styles.js
@@ -1,21 +1,45 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
+    padding: 10
   },
   title: {
     fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 15
   },
   movieColumnContainer: {
     marginRight: 20,
-    width: 150,
+    width: 150
   },
+  movieColumnTitle: {
+    fontWeight: 'bold',
+    marginVertical: 15
+  },
+
+
   movieColumnImage: {
     width: 150,
     height: 200,
-    borderRadius: 10,
+    borderRadius: 10
   },
+  movieRowContainer: {
+    flexDirection: 'row',
+    marginBottom: 20
+  },
+  movieRowTitle: {
+    fontWeight: 'bold'
+  },
+  movieRowImage: {
+    width: 75, 
+    height: 100 ,
+  },
+
+  movieInfoContainer: {
+    marginLeft: 10,
+    justifyContent: "space-between"
+  }
 });
 
 export default styles;
